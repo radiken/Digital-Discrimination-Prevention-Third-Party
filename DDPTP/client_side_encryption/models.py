99a37acf49427daf044329852e22d9950441c218
Zhuolun Li <zhuolun.li@kcl.ac.uk>
2020-12-04 15:41:46 +0000

    alter models

--- DDPTP/client_side_encryption/models.py
+++ DDPTP/client_side_encryption/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Individual(models.Model):
-	title = models.TextField(max_length=50)
-	name = models.TextField(max_length=100)
-	gender = models.TextField()
-	age = models.DecimalField(decimal_places=0, max_digits=100)
+	title = models.CharField(max_length=20)
+	name = models.CharField(max_length=50)
+	gender = models.CharField(max_length=20)
+	age = models.DecimalField(decimal_places=0, max_digits=3)
