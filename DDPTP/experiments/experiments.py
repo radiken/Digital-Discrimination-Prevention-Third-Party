50524d36399a9d4c421bf51e33d17061ea940c19
Zhuolun Li <zhuolun.li@kcl.ac.uk>
2021-04-04 01:42:50 +0100

    update discriptive texts

--- DDPTP/experiments/experiments.py
+++ DDPTP/experiments/experiments.py
@@ -139,7 +139,6 @@ def adult_prediction(actions):
     processed_test_x = processed_test_x[processed_columns]
     processed_result = clf.predict(processed_test_x)
     processed_score = clf.score(processed_test_x, test_y)
-    print(processed_score)
 
     sex_original_metrics = get_result_metrics(test_x, original_result, test_y, "sex", ["Male"], ["Female"], 1, 0)
     sex_processed_metrics = get_result_metrics(test_x, processed_result, test_y, "sex", ["Male"], ["Female"], 1, 0)
@@ -315,111 +314,6 @@ def epsilon_and_noise_level_chart_figures():
     for epsilon in np.arange(0.1, 10.0, 0.1):
         figures.append([epsilon, 1/epsilon])
     return figures
-
-'''
-Experiment 3
-Test whether the app can verify the fairness of the algorithms
-Based on the distribution of the sensitive information in the classification
-
-
-experiment 3 redesign:
-1. test discrimination level when only remove sensitive attributes
-2. if not good, compute the correlations, find the most related attributes
-3. remove the relevant attributes and try again
-'''
-
-def get_statlog_models_sensitive_rates(x, y):
-    x = pd.DataFrame(x)
-    x.columns = ["account_status", "duration", "credit_history", "purpose", "credit_amount", "savings_account", "present_employment_since", "installment_rate_in_income", "personal_status_and_sex", 
-        "guarantors", "present_residence_since", "property", "age", "other_installment_plans", "housing", "existing_credits", "job", "maintenance_provider_number", "telephone", "foreign_worker"]
-
-    original_train_x, original_test_x, train_y, test_y = train_test_split(x, list(y), test_size=0.33, random_state=0)
-
-    # remove sensitive information in the processed training set
-    processed_train_x = original_train_x.drop(columns=["personal_status_and_sex", "age"])
-    processed_test_x = original_test_x.drop(columns=["personal_status_and_sex", "age"])
-
-    # one hot encode
-    one_hot_original_train_x = pd.get_dummies(original_train_x, columns=["account_status", "credit_history", "purpose", "savings_account", "present_employment_since", "personal_status_and_sex", 
-        "guarantors", "property", "other_installment_plans", "housing", "job", "telephone", "foreign_worker"])
-    one_hot_original_test_x = pd.get_dummies(original_test_x, columns=["account_status", "credit_history", "purpose", "savings_account", "present_employment_since", "personal_status_and_sex", 
-        "guarantors", "property", "other_installment_plans", "housing", "job", "telephone", "foreign_worker"])
-    one_hot_processed_train_x = pd.get_dummies(processed_train_x, columns=["account_status", "credit_history", "purpose", "savings_account", "present_employment_since", 
-        "guarantors", "property", "other_installment_plans", "housing", "job", "telephone", "foreign_worker"])
-    one_hot_processed_test_x = pd.get_dummies(processed_test_x, columns=["account_status", "credit_history", "purpose", "savings_account", "present_employment_since", 
-        "guarantors", "property", "other_installment_plans", "housing", "job", "telephone", "foreign_worker"])
-
-    original_clf = tree.DecisionTreeClassifier()
-    original_clf.fit(one_hot_original_train_x, train_y)
-    original_result = original_clf.predict(one_hot_original_test_x)
-    original_result = [i-1 for i in original_result]
-    original_zero_rates, original_one_rates = get_model_prediction_attribute_distribution(original_test_x, original_result, "personal_status_and_sex")
-
-    processed_clf = tree.DecisionTreeClassifier()
-    processed_clf.fit(one_hot_processed_train_x, train_y)
-    processed_result = processed_clf.predict(one_hot_processed_test_x)
-    processed_result = [i-1 for i in processed_result]
-    processed_zero_rates, processed_one_rates = get_model_prediction_attribute_distribution(original_test_x, processed_result, "personal_status_and_sex")
-
-    return original_zero_rates, original_one_rates, processed_zero_rates, processed_one_rates
-
-
-
-def get_adult_models_sensitive_rates(test_x):
-    # preprocess data
-    test_x = pd.DataFrame(list(test_x))
-
-    # handle missing values
-    imp = SimpleImputer(missing_values="?", strategy="most_frequent")
-    test_x = pd.DataFrame(imp.fit_transform(test_x))
-
-    # remove with gender, race, native country and marital status for the processed data
-    test_x.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country']
-    processed_test_x = test_x.drop(columns=['marital_status', 'race', 'sex', 'native_country'])
-    # abstract age(young age:<=35, middle age: 36-55, older age: >=56) and relationship(husband: husband-or-wife, wife: husband-or-wife)
-    abstracted_test_x = processed_test_x.copy()
-    for i in range(len(abstracted_test_x)):
-        age = abstracted_test_x.at[i, 'age']
-        if age <= 35:
-            abstracted_test_x.at[i, 'age'] = "young_age"
-        elif 36 <= age <= 55:
-            abstracted_test_x.at[i, 'age'] = "middle_age"
-        else:
-            abstracted_test_x.at[i, 'age'] = "older_age"
-
-        relationship = abstracted_test_x.at[i, 'relationship']
-        if relationship == 'Husband' or relationship == 'Wife':
-            abstracted_test_x.at[i, 'relationship'] = "Husband-or-wife"
-
-    # one hot encode
-    original_test_x = pd.get_dummies(test_x, columns=['workclass', 'education', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 'native_country'])
-    processed_test_x = pd.get_dummies(processed_test_x, columns=['workclass', 'education', 'occupation', 'relationship'])
-    abstracted_test_x = pd.get_dummies(abstracted_test_x, columns=['age', 'workclass', 'education', 'occupation', 'relationship'])
-
-    # match columns(features)
-    original_columns = pickle.load(open('ml_models/adult_original_columns', 'rb'))
-    original_missing_columns = set(original_columns) - set(original_test_x.columns)
-    for column in original_missing_columns:
-        original_test_x[column] = 0
-    original_test_x = original_test_x[original_columns]
-
-    original_result = load_model_and_predict('original_decision_tree', original_test_x)
-    processed_result = load_model_and_predict('processed_decision_tree', processed_test_x)
-    abstracted_result = load_model_and_predict('abstracted_decision_tree', abstracted_test_x)
-    
-    original_zero_rates = {}
-    original_one_rates = {}
-    processed_zero_rates = {}
-    processed_one_rates = {}
-    abstracted_zero_rates = {}
-    abstracted_one_rates = {}
-    # zero and one represents the classification result 0 and 1
-    for attribute in ['sex', 'race', 'native_country', 'marital_status']:
-        original_zero_rates[attribute], original_one_rates[attribute] = get_model_prediction_attribute_distribution(test_x, original_result, attribute)
-        processed_zero_rates[attribute], processed_one_rates[attribute] = get_model_prediction_attribute_distribution(test_x, processed_result, attribute)
-        abstracted_zero_rates[attribute], abstracted_one_rates[attribute] = get_model_prediction_attribute_distribution(test_x, abstracted_result, attribute)
-
-    return original_zero_rates, original_one_rates, processed_zero_rates, processed_one_rates, abstracted_zero_rates, abstracted_one_rates
     
 def load_model_and_predict(model_name, one_hot_test_x):
     model = pickle.load(open(f'ml_models/{model_name}', 'rb'))
